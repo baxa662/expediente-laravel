a0fd51fd8f0d668ba9e7ca36ec7089dbfc35015e
baxa662 <avilesjuan2406@gmail.com>
2024-01-21 18:14:42 -0600

    Cambio de servidor

--- resources/js/services/GlobalService.jsx
+++ resources/js/services/GlobalService.jsx
@@ -3,32 +3,34 @@ import axios from "axios";
 async function peticion(params, url) {
   const token = localStorage.getItem("sessionToken");
 
-  return axios
-    .post(`http://127.0.0.1:8000/api/${url}`, params, {
-      // .post(`https://umerexp.ddns.net/api/${url}`, params, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        withCredentials: true,
-      },
-    })
-    .then((response) => {
-      if (response.status == 200) {
-        return response.data;
-      } else if (response.status == 204) {
+  return (
+    axios
+      // .post(`http://127.0.0.1:8000/api/${url}`, params, {
+      .post(`https://umerexp.ddns.net/api/${url}`, params, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          withCredentials: true,
+        },
+      })
+      .then((response) => {
+        if (response.status == 200) {
+          return response.data;
+        } else if (response.status == 204) {
+          return false;
+        } else {
+          return response.data;
+        }
+      })
+      .catch((error) => {
+        if (error.response.status == 401) {
+          localStorage.clear();
+          location.reload();
+        } else if (error.response.status == 400) {
+          return error.response.data;
+        }
         return false;
-      } else {
-        return response.data;
-      }
-    })
-    .catch((error) => {
-      if (error.response.status == 401) {
-        localStorage.clear();
-        location.reload();
-      } else if (error.response.status == 400) {
-        return error.response.data;
-      }
-      return false;
-    });
+      })
+  );
 }
 
 export default peticion;
